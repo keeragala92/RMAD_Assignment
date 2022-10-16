d2dcc1a0bf3af758b2667817f2f344b8c417a159
Isuru Sampath <kasamee92@gmail.com>
2022-10-16 14:41:51 +0900

    socond commint

--- index.js
+++ index.js
@@ -5,7 +5,8 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+import Assignment from './src/Assignment';
 import HomeCC from './src/HomeCC';
 
-AppRegistry.registerComponent(appName, () => HomeCC);
+AppRegistry.registerComponent(appName, () =>Assignment);
 
